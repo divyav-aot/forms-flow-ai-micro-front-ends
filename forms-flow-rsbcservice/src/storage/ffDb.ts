32f3b1ffd9027a146c70b20e0270cd1b6c5ea78b
divyav-aot <divyav-aot@github>
2025-03-05 16:46:58 -0500

    order by modified date for drafts

--- forms-flow-rsbcservice/src/storage/ffDb.ts
+++ forms-flow-rsbcservice/src/storage/ffDb.ts
@@ -197,7 +197,7 @@ class FormsFlowDB extends Dexie {
       formDefinitionList: "id, formId, formName, formType, processKey, modified",
       applications: "id, formId, submissionId",
       drafts: "id, applicationId, formId",
-      offlineSubmissions: "_id, formId, localSubmissionId, localDraftId, type",
+      offlineSubmissions: "_id, formId, localSubmissionId, localDraftId, type, modified",
       formDefinitions: "_id, title, name, path, type, created, modified, machineName, parentFormId",
       activeForm: "localDraftId, serverDraftId",
       formProcesses: "formId, formName"
